3056db573cf07bec7a4e933327136a5d5ed77e67
Le-Tien-Dung <letiendung911@gmail.com>
2025-04-07 11:43:00 +0700

    Add files via upload

---
+++ array/Untitled20.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int n = 10;
+    int a[10] = {3, 2, 1, 4, 5, 8, 9, 7, 6, 10};
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    for(int i = n ; i >= 0; i--){
+        printf("%d ", a[i]);
+    }
+}

6607b74e47ec1edf432c2baf357c2da594a8eeb7
Le-Tien-Dung <letiendung911@gmail.com>
2025-04-07 15:12:07 +0700

    Add files via upload

---
+++ array/Untitled24.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void nhapham(float a[],int* n){
+    printf("nhap kich thuoc mang ");
+    scanf("%d", n);
+    printf("nhap mang ");
+    for (int i =0;i<*n;i++)
+    {
+        scanf("%f", &a[i]);
+    }
+}
+int main() {
+	int n;
+	float a[1000];
+	nhapham(a,&n);
+	for(int i=0;i<n;i++){
+		if(a[i]<0){
+			a[i]=0;
+		}
+	}
+	for(int i=0;i<n;i++){
+		printf("mang sau khi thay la %.1f\n",a[i]);
+	}
+}
